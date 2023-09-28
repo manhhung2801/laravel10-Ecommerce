2d07be5bdd47155d7d416c1c032afbc917bf73a3
manhhung2801 <hung28012002zz@gmail.com>
2023-09-28 20:58:45 +0700

    Multi auth - Create middleware for multi auth

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->middleware('auth')->name('admin.dashboard');
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->middleware('auth', 'role:admin')->name('admin.dashboard');
 
 // Vendor rooute
-Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->middleware('auth')->name('vendor.dashboard');
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->middleware('auth', 'role:vendor')->name('vendor.dashboard');
