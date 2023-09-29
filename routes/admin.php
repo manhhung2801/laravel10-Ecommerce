de0312320eec5749f82a560cf860135d6ba74253
manhhung2801 <hung28012002zz@gmail.com>
2023-09-29 15:56:36 +0700

    Admin profile update 1

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,13 @@
 <?php 
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // Admin route
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+// Profile route
+
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
